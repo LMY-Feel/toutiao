3ed0ec9550812b1948aceb32721f21fc2c319832
lin ming yao <910548749@qq.com>
2019-08-25 12:14:35 +0800

    导航栏和404处理完毕

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 
 Vue.use(VueRouter)
 
@@ -10,7 +13,16 @@ const router = new VueRouter({
   routes: [
     //   name选项作用：找到对应路由规则
     //   跳转方便一些：$router.push('/login') 或者 $router.push({name:'login'})
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/',
+      component: Home,
+      children: [
+        // 路由规则：子路由有名称，父路由不需要设置name属性
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    },
+    { path: '*', name: '404', component: NotFound }
   ]
 })
 
